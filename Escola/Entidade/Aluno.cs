5470d2fbeeddebdcbd3f750177f725c7102f90e1
daegrick <lucas.cabelaum@gmail.com>
2025-02-05 21:04:37 -0300

    Adicionar arquivos de projeto.

---
+++ Escola/Entidade/Aluno.cs
@@ -0,0 +1,20 @@
+﻿namespace Escola.Entidade
+{
+    public class Aluno
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Classe { get; set; }
+        public DateTime? DataNascimento { get; set; }
+        public DateTime? AlteradoEm { get; set; }
+
+        public Aluno()
+        {
+            Id = Guid.Empty;
+            Nome =
+                Classe = string.Empty;
+            AlteradoEm = null;
+            DataNascimento = null;
+        }
+    }
+}
---
+++ Escola/Entidade/FolhaPresenca.cs
@@ -0,0 +1,22 @@
+﻿namespace Escola.Entidade
+{
+    public class FolhaPresenca
+    {
+        public Guid Id { get; set; }
+        public Aluno Aluno { get; set; }
+        public DateTime? Data { get; set; }
+        public int Aulas { get; set; }
+        public bool PossuiAtestadoFalta { get; set; }
+        public decimal PresencaNaAula { get; set; }
+
+        public FolhaPresenca()
+        {
+            Id = Guid.Empty;
+            Aluno = new Aluno();
+            Data = null;
+            Aulas = 0;
+            PossuiAtestadoFalta = false;
+            PresencaNaAula = 0;
+        }
+    }
+}
